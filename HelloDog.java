fc89258f69ae9492ad0d6c12db3227ecb1efebbc
chenjunwei123 <1294511002@qq.com>
2023-06-06 21:15:06 +0800

    first commit

---
+++ HelloDog.java
@@ -0,0 +1,11 @@
+/**
+ * ClassName: HelloDog
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author cjw
+ * @Create 2023/4/20 19:34
+ * @Version 1.0
+ */
+public class HelloDog {
+}
---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String [] args){
+
+        System.out.println("HelloWorld");
+        Scanner scan = new Scanner(System.in);
+        int histioy = scan.nextInt();
+        System.out.println(histioy);
+    }
+    public void method(){
+        Math.random();
+        /*String.join();*/
+
+
+    }
+}
